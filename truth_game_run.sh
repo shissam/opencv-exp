0fd277f187890eac03588634eedb97d25423e988
scott hissam <shissam@users.noreply.github.com>
2019-12-15 14:44:44 -0600

    Update truth_game_run.sh

--- truth_game_run.sh
+++ truth_game_run.sh
@@ -1,2 +1,74 @@
 #!/bin/bash
+
+cat << TESTTRUTH > /tmp/testtruth.txt
+[INFO] images/game01.jpg: person: 0.9992
+[INFO] images/game01.jpg: person: 0.9946
+[INFO] images/game02.jpg: person: 0.9987
+[INFO] images/game03.jpg: person: 0.9921
+[INFO] images/game03.jpg: person: 0.9918
+[INFO] images/game04.jpg: person: 0.9966
+[INFO] images/game04.jpg: backpack: 0.7760
+[INFO] images/game05.jpg: person: 0.9995
+[INFO] images/game05.jpg: person: 0.9940
+[INFO] images/game05.jpg: dog: 0.9376
+[INFO] images/game06.jpg: person: 0.9904
+[INFO] images/game07.jpg: person: 0.5305
+[INFO] images/game08.jpg: 
+[INFO] images/game09.jpg: 
+[INFO] images/game10.jpg: person: 0.9994
+[INFO] images/game11.jpg: person: 0.9908
+[INFO] images/game12.jpg: person: 0.9346
+[INFO] images/game12.jpg: person: 0.8550
+[INFO] images/game13.jpg: person: 0.7329
+[INFO] images/game14.jpg: person: 0.8466
+[INFO] images/game15.jpg: person: 0.9972
+[INFO] images/game16.jpg: person: 0.9983
+[INFO] images/game17.jpg: person: 0.9057
+[INFO] images/game18.jpg: person: 0.7802
+[INFO] images/game19.jpg: 
+[INFO] images/game20.jpg: person: 0.7992
+[INFO] images/game21.jpg: person: 0.9971
+[INFO] images/game21.jpg: person: 0.9940
+[INFO] images/game22.jpg: person: 0.9997
+[INFO] images/game22.jpg: person: 0.9991
+[INFO] images/game22.jpg: person: 0.9972
+[INFO] images/game22.jpg: person: 0.9873
+[INFO] images/game23.jpg: person: 0.9963
+[INFO] images/game23.jpg: person: 0.9853
+[INFO] images/game23.jpg: person: 0.9777
+[INFO] images/game23.jpg: person: 0.9761
+[INFO] images/game23.jpg: person: 0.9735
+[INFO] images/game23.jpg: person: 0.9464
+[INFO] images/game23.jpg: person: 0.9286
+[INFO] images/game23.jpg: car: 0.8829
+[INFO] images/game23.jpg: person: 0.8301
+[INFO] images/game23.jpg: car: 0.8029
+[INFO] images/game24.jpg: person: 0.9960
+[INFO] images/game24.jpg: person: 0.5114
+[INFO] images/game25.jpg: person: 0.9973
+[INFO] images/game25.jpg: person: 0.9590
+[INFO] images/game25.jpg: person: 0.9068
+[INFO] images/game26.jpg: 
+[INFO] images/game27.jpg: person: 0.9944
+[INFO] images/game27.jpg: dog: 0.6445
+[INFO] images/game28.jpg: person: 0.9915
+[INFO] images/game28.jpg: person: 0.6773
+[INFO] images/game28.jpg: car: 0.5405
+[INFO] images/game29.jpg: person: 0.9895
+[INFO] images/game29.jpg: person: 0.6728
+[INFO] images/game30.jpg: tvmonitor: 0.7217
+[INFO] images/game31.jpg: bed: 0.9966
+[INFO] images/game32.jpg: person: 0.9721
+[INFO] images/game32.jpg: person: 0.9637
+[INFO] images/game32.jpg: person: 0.9013
+[INFO] images/game32.jpg: person: 0.8678
+[INFO] images/game32.jpg: person: 0.8674
+[INFO] images/game32.jpg: person: 0.7822
+[INFO] images/game32.jpg: person: 0.5985
+[INFO] images/game32.jpg: chair: 0.5126
+[INFO] images/game32.jpg: person: 0.5069
+[INFO] images/game33.jpg: person: 0.9933
+[INFO] images/game33.jpg: person: 0.7195
+TESTTRUTH
+
 exit 0
